744713dbc9e94fd23ab348aded6353a423fab0d5
ma34s <ma34s.sss@gmail.com>
2013-05-17 01:09:59 +0900

    proprietary files get from device by adb

--- extract-files.sh
+++ extract-files.sh
@@ -30,8 +30,8 @@ for FILE in `cat proprietary-files.txt | grep -v ^# | grep -v ^$`; do
     if [ ! -d ../../../vendor/$VENDOR/$DEVICE/proprietary/$DIR ]; then
         mkdir -p ../../../vendor/$VENDOR/$DEVICE/proprietary/$DIR
     fi
-    #adb pull /$FILE ../../../vendor/$VENDOR/$DEVICE/proprietary/$FILE
-    cp -av $1/$FILE ../../../vendor/$VENDOR/$DEVICE/proprietary/$FILE
+    adb pull /$FILE ../../../vendor/$VENDOR/$DEVICE/proprietary/$FILE
+    
 done
 
 (cat << EOF) | sed s/__DEVICE__/$DEVICE/g | sed s/__VENDOR__/$VENDOR/g > ../../../vendor/$VENDOR/$DEVICE/$DEVICE-vendor-blobs.mk
